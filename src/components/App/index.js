cf97f8be2866fbff78f480b3df91f21fb5eb4108
abhinavk454 <abhinav.vikash2@gmail.com>
2020-04-27 14:24:07 +0530

    added suburls

---
+++ src/components/App/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignInPage from '../SignIn';
+import SignOutPage from '../SignOut';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+import PasswordForgetPage from '../PasswordChange';
+import * as ROUTES from '../../constants/routes';
+
+const App=()=>(
+    <Router>
+        <div>
+            <Navigation />
+            <hr />
+            <Route exact path={ROUTES.LANDING} component={LandingPage}/>
+            <Route path={ROUTES.ADMIN} component={AdminPage}/>
+            <Route path={ROUTES.ACCOUNT} component={AccountPage}/>
+            
+        </div>
+    </Router>
+);
+
+export default App;
