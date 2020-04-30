de520a8d4e4c9cbab369257ed4d1852ea4e19cbf
abhinavk454 <abhinav.vikash2@gmail.com>
2020-04-30 10:52:07 +0530

    done passwoed forget

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,52 +1,60 @@
-import React,{Component} from 'react';
-import {withRouter} from 'react-router-dom';
-import {compose} from 'recompose';
-import {SignUpLink} from '../SignUp';
-import {withFirebase} from '../Firebase';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+
+import { SignUpLink } from '../SignUp';
+import { PasswordForgetLink } from '../PasswordForget';
+import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 const SignInPage = () => (
   <div>
     <h1>SignIn</h1>
     <SignInForm />
+    <PasswordForgetLink />
     <SignUpLink />
   </div>
 );
 
-const INITIAL_STATE={
-  email:'',
-  password:'',
-  error:null,
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null,
 };
 
-class SignInFormBase extends Component{
-  constructor(props){
+class SignInFormBase extends Component {
+  constructor(props) {
     super(props);
-    this.state={...INITIAL_STATE};
+
+    this.state = { ...INITIAL_STATE };
   }
 
-  onSubmit=event=>{
-    const {email,password}=this.state;
+  onSubmit = event => {
+    const { email, password } = this.state;
+
     this.props.firebase
-      .doSignInWithEmailAndPassword(email,password)
-      .then(()=>{
-        this.setState({...INITIAL_STATE});
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
         this.props.history.push(ROUTES.HOME);
-      }) 
-      .catch(error=>{
-        this.setState({error});
+      })
+      .catch(error => {
+        this.setState({ error });
       });
-      event.preventDefault();
+
+    event.preventDefault();
   };
 
-  onChange=event=>{
-    this.setState({[event.target.name]:event.target.value});
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
   };
 
-  render(){
-    const {email,password,error}=this.state;
-    const isInvalid=password === '' || email === '';
-    return(
+  render() {
+    const { email, password, error } = this.state;
+
+    const isInvalid = password === '' || email === '';
+
+    return (
       <form onSubmit={this.onSubmit}>
         <input
           name="email"
@@ -65,17 +73,18 @@ class SignInFormBase extends Component{
         <button disabled={isInvalid} type="submit">
           Sign In
         </button>
+
         {error && <p>{error.message}</p>}
       </form>
     );
   }
 }
 
-const SignInForm=compose(
+const SignInForm = compose(
   withRouter,
   withFirebase,
 )(SignInFormBase);
 
 export default SignInPage;
 
-export {SignInForm};
+export { SignInForm };
